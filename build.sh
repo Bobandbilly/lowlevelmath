54f8c1fcd2c688bdcc8f4d1b934fb37050337804
Bob <166648410+Bobandbilly@users.noreply.github.com>
2025-03-18 08:10:56 +0000

    Re-making build file

--- build.sh
+++ build.sh
@@ -2,15 +2,14 @@
 
 set -e
 
-cd src
-mkdir -pv ../dist/
-gcc -shared -O3 -march=native -fPIC -o ../dist/lowleveladd.so lowleveladd.c
-sudo cp -v ../dist/lowleveladd.so /usr/lib/lowleveladd-x86_64.so.6
+mkdir -pv ./dist/
+gcc -shared -O3 -march=native -fPIC -o ./dist/lowleveladd.so lowleveladd.c
+sudo cp -v ./dist/lowleveladd.so /usr/lib/lowleveladd-x86_64.so.6
 sudo ln -s /usr/lib/lowleveladd-x86_64.so.6 /usr/lib/liblowleveladd.so
 echo "/usr/lib" | sudo tee -a /etc/ld.so.conf.d/lowleveladd.conf
 sudo ldconfig
-sudo cp ../include/lowleveladd.h /usr/include/linux/lowleveladd.h
+sudo cp ./include/lowleveladd.h /usr/include/linux/lowleveladd.h
 sudo bash -c 'echo -e "prefix=/usr\nexec_prefix=\${prefix}\nlibdir=\${exec_prefix}/lib\nincludedir=\${prefix}/include/linux\n\nName: lowleveladd\nDescription: Critical Linux Addition API\nVersion: 6.9.420\nCflags: -I\${includedir}\nLibs: -L\${libdir} -llowleveladd" > /usr/lib/pkgconfig/lowleveladd.pc'
-sudo cp ../include/lowleveladd.h ../dist/
+sudo cp ../include/lowleveladd.h ./dist/
 echo Done! Builded files located in /usr/include/, /lib and in dist/
 exit 0
